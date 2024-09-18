4d481a5d9998c679b37d11f8eb91f56796db22a5
Daniel Wendel <52021012+draago10@users.noreply.github.com>
2024-09-17 20:52:49 -0400

    Testing API

--- app/src/main/java/com/example/wrinklethinkle/network/APIService.kt
+++ app/src/main/java/com/example/wrinklethinkle/network/APIService.kt
@@ -1,5 +1,5 @@
 import retrofit2.Call
-import retrofit2.GET
+import retrofit2.http.GET
 import retrofit2.http.Query
 import com.example.wrinklethinkle.model.PlantResponse
 
@@ -8,9 +8,9 @@ interface APIService {
 
     // This function sends a GET request to the /plants/search endpoint with a query parameter
     // for the search term (e.g., "coconut") and the API token.
-    @GET("plants/search")
+    @GET("plants")
     fun searchPlants(
-        @Query("token") token: String,    // Query parameter for the API token
-        @Query("q") query: String         // Query parameter for the search term (e.g., plant name)
+        @Query("token") token: String,
+        @Query("filter[common_name]") commonName: String // This will map to filter[common_name]
     ): Call<PlantResponse>                // Retrofit will return a Call object with PlantResponse
 }

618ad1814a1aa1be3ff0248e126487ec74fd070b
A-A-Ron-Woods <159043365+A-A-Ron-Woods@users.noreply.github.com>
2024-09-19 14:08:20 -0700

    Added firebase support framework

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    //id("com.android.application")
+    //id("com.google.gms.google-services")
 }
 
 android {
@@ -48,5 +51,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
 
 }

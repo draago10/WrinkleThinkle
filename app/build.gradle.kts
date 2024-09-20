ec427190b597f0eda10f9621f3bb5dfe31aa2bbb
A-A-Ron-Woods <159043365+A-A-Ron-Woods@users.noreply.github.com>
2024-09-20 09:42:22 -0700

    Re-setup firebase to a team email

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("com.google.gms.google-services") version "4.4.2" apply false
-    //id("com.android.application")
-    //id("com.google.gms.google-services")
+
 }
 
 android {

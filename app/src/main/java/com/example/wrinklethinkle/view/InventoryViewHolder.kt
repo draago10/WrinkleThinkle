f9253128953c1b9f4c5eb750a1ce55c1db9b88c2
Perle Alkhodari <perle.alkhodari@gmail.com>
2024-10-04 01:48:28 +0300

    added class

--- app/src/main/java/com/example/wrinklethinkle/view/InventoryViewHolder.kt
+++ app/src/main/java/com/example/wrinklethinkle/view/InventoryViewHolder.kt
@@ -2,10 +2,12 @@ package com.example.wrinklethinkle.view
 
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wrinklethinkle.R
 
 
 class InventoryViewHolder(view : View) : RecyclerView.ViewHolder(view) {
     val itemImage: ImageView = view.findViewById(R.id.imageView5);
+    val itemAmound: TextView = view.findViewById(R.id.textView2);
 }

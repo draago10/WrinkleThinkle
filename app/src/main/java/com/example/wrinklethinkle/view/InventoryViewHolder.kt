527656a8a211507c66e36f784c942511bfa7909c
Perle Alkhodari <perle.alkhodari@gmail.com>
2024-10-04 01:47:18 +0300

    designed inventory item

---
+++ app/src/main/java/com/example/wrinklethinkle/view/InventoryViewHolder.kt
@@ -0,0 +1,11 @@
+package com.example.wrinklethinkle.view
+
+import android.view.View
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wrinklethinkle.R
+
+
+class InventoryViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+    val itemImage: ImageView = view.findViewById(R.id.imageView5);
+}

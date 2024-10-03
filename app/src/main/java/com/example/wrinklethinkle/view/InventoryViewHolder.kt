50f884a6fc8a6aaca85f7acf23fddd3a47c34a58
Perle Alkhodari <perle.alkhodari@gmail.com>
2024-10-04 01:56:59 +0300

    added another class and fixed a typo

--- app/src/main/java/com/example/wrinklethinkle/view/InventoryViewHolder.kt
+++ app/src/main/java/com/example/wrinklethinkle/view/InventoryViewHolder.kt
@@ -1,6 +1,8 @@
 package com.example.wrinklethinkle.view
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -9,5 +11,16 @@ import com.example.wrinklethinkle.R
 
 class InventoryViewHolder(view : View) : RecyclerView.ViewHolder(view) {
     val itemImage: ImageView = view.findViewById(R.id.imageView5);
-    val itemAmound: TextView = view.findViewById(R.id.textView2);
+    val itemAmount: TextView = view.findViewById(R.id.textView2);
+}
+
+class InventoryAdapter(val inventory:List<String>) : RecyclerView.Adapter<InventoryViewHolder>() {
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): InventoryViewHolder {
+        val view = LayoutInflater.from(p0.context).inflate(R.layout.inventory_custom_cell, p0, false);
+        return InventoryViewHolder(view)
+    }
+
+    override fun onBindViewHolder(p0: InventoryViewHolder, p1: Int) {
+        val item = inventory[p1]
+    }
 }

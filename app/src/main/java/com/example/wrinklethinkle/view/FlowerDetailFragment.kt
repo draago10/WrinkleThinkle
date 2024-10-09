261cdfcbf67f3cb2ad1746a51e43e5982ee6f2ec
Daniel Wendel <52021012+draago10@users.noreply.github.com>
2024-10-08 21:55:49 -0400

    Finished the flower detail screen
    Added all the flower images as well

--- app/src/main/java/com/example/wrinklethinkle/view/FlowerDetailFragment.kt
+++ app/src/main/java/com/example/wrinklethinkle/view/FlowerDetailFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import com.example.wrinklethinkle.R
 import com.example.wrinklethinkle.Utility.Utility
 import com.example.wrinklethinkle.databinding.FragmentFlowerDetailBinding
 import com.example.wrinklethinkle.model.getFlowerDetails
+import kotlin.random.Random
 
 class FlowerDetailFragment : Fragment() {
     private var flowerDetailFragmentBinding: FragmentFlowerDetailBinding? = null
@@ -27,29 +29,46 @@ class FlowerDetailFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val flower = arguments?.getString("flower") ?: return
         val flowerData = getFlowerDetails(requireContext(), flower)
+        var currentImageIndex = 0
+        val shrinkGrowAnimation = AnimationUtils.loadAnimation(context, R.anim.shrink_and_grow)
         if (flowerData != null) {
-            binding.flowerImage.setImageResource(mapImage(flower))
+            binding.flowerImage.setImageResource(mapImage(flower)[0])
+            binding.flowerScientificName.text = "Scientific Name: \t ${flowerData.scientificName}"
             binding.flowerName.text = flowerData.name
+            binding.flowerColors.text = "Colors: \t"
             for (color in flowerData.colors) {
-                binding.flowerColors.append(color + "\n")
+                binding.flowerColors.append(color + ", ")
             }
-            binding.flowerSymbolism.text = flowerData.symbolism
-            binding.flowerFamily.text = flowerData.family
-            binding.flowerHeight.text = flowerData.averageHeightCm.toString()
-            binding.flowerBloomSeason.text = flowerData.bloomSeason
+            binding.flowerOrigin.text = "Origin: \t ${flowerData.origin}"
+            binding.flowerSymbolism.text = "Symbolism: \t ${flowerData.symbolism}"
+            binding.flowerFamily.text = "Flower Family: \t ${flowerData.family}"
+            binding.flowerHeight.text = "Flower Height: \t ${flowerData.averageHeightCm} cm"
+            binding.flowerBloomSeason.text = "Bloom Season: \t ${flowerData.bloomSeason}"
             binding.flowerDescription.text = flowerData.description
+            binding.flowerImage.setOnClickListener {
+                binding.flowerImage.startAnimation(shrinkGrowAnimation)
+                if (currentImageIndex != mapImage(flower).size) {
+                    binding.flowerImage.setImageResource(mapImage(flower)[currentImageIndex])
+                    currentImageIndex++
+                } else {
+                    currentImageIndex = 0
+                }
+
+            }
         } else {
             Utility().showErrorPopup(childFragmentManager, requireContext(), R.drawable.project_gnome,"Oh my gnome!", "We could not load the flower data.")
 
         }
     }
 
-    fun mapImage(flowerName: String): Int {
+    fun mapImage(flowerName: String): List<Int> {
         return when(flowerName) {
-            "ROSE" -> R.drawable.real_rose
-            else -> R.drawable.error_screen_cat
+            "ROSE" -> listOf(R.drawable.rose_flower_real, R.drawable.rose_flower_complete)
+            "TULIP" -> listOf(R.drawable.tulip_flower_real, R.drawable.tulip_flower_complete)
+            "BLACK DAHLIA" -> listOf(R.drawable.black_dahlia_real, R.drawable.black_dahlia_flower_complete)
+            "LILY" -> listOf(R.drawable.lily_flower_real, R.drawable.lily_flower_complete)
+            else -> listOf(R.drawable.error_screen_cat, R.drawable.project_gnome)
         }
-
     }
 
 
